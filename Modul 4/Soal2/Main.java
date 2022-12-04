c565b7ca3341611d027f26b44c202a4e8a3d2a6f
hennykartika <93182127+hennykartika@users.noreply.github.com>
2022-12-04 23:50:00 +0800

    Update Main.java

--- Modul 4/Soal2/Main.java
+++ Modul 4/Soal2/Main.java
@@ -1,59 +1,65 @@
-package Praktikum4.Soal2;
-import java.util.Scanner;
+package praktikum4.soal2;
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Pilih buku yang ingin diinputkan");
+        int inputUser;
+        
+        // Instansiasi objek input dari Scanner
+        Scanner input = new Scanner(System.in);
+        
+        // Input User
+        System.out.println("Pilih buku yang ingin diinputkan:");
         System.out.println("1 = Novel");
         System.out.println("2 = Komik");
-        System.out.print("Masukkan Pilihan : ");
-        int pil = inp.nextInt();
-        inp.nextLine();
-
-        if (pil == 1){
-            String judul, penulis, tahun, genre, sinopsis;
+        System.out.print("Masukkan pilihan: ");
+        inputUser = input.nextInt();
+        input.nextLine();
+        
+        // Kondisi Novel / Komik
+        if( inputUser == 1 ) {
+            String judul, penulis, genre, sinopsis, tahun;
             System.out.print("Judul: ");
-            judul = inp.nextLine();
-
-            System.out.print("Penulis: ");
-            penulis = inp.nextLine();
-
-            System.out.print("Tahun Terbit: ");
-            tahun = inp.nextLine();
-
-            System.out.print("Genre: ");
-            genre = inp.nextLine();
-
-            System.out.print("Sinopsis: ");
-            sinopsis = inp.nextLine();
-
-            Novel nov = new Novel(judul, penulis, tahun, genre, sinopsis);
-            System.out.println(nov.getNovelDetail());
-        }
-        else if (pil == 2){
-            String judul, penulis, tahun, sinopsis;
+            judul = input.nextLine();
+            
+            System.out.print("Penulis : ");
+            penulis = input.nextLine();
+            
+            System.out.print("Tahun Terbit : ");
+            tahun = input.nextLine();
+            
+            System.out.print("Genre : ");
+            genre = input.nextLine();
+            
+            System.out.print("Sinopsis : ");
+            sinopsis = input.nextLine();
+            
+            // Instansiasi Objek
+            Novel novel = new Novel(judul, penulis, tahun, genre, sinopsis);
+            System.out.println(novel.getNovelDetail());
+            
+        } else if ( inputUser == 2 ) {
+            String judul, penulis, sinopsis, tahun;
             int volume;
             System.out.print("Judul: ");
-            judul = inp.nextLine();
-
-            System.out.print("Penulis: ");
-            penulis = inp.nextLine();
-
-            System.out.print("Tahun Terbit: ");
-            tahun = inp.nextLine();
-
-            System.out.print("Volume: ");
-            volume = inp.nextInt();
-
-            inp.nextLine();
-            System.out.print("Sinopsis: ");
-            sinopsis = inp.nextLine();
-
-            Komik kom = new Komik(judul, penulis, tahun, volume, sinopsis);
-            System.out.println(kom.getKomikDetail());
-        } else{
-            System.out.println("Tidak ada di daftar pilihan");
-        }
+            judul = input.nextLine();
+            
+            System.out.print("Penulis : ");
+            penulis = input.nextLine();
+            
+            System.out.print("Tahun Terbit : ");
+            tahun = input.nextLine();
+            
+            System.out.print("Volume : ");
+            volume = input.nextInt();
+            input.nextLine();
+            
+            System.out.print("Sinopsis : ");
+            sinopsis = input.nextLine();
+            
+            // Deklarasi objek
+            Komik komik = new Komik(judul, penulis, tahun, volume, sinopsis);
+            System.out.println(komik.getKomikDetail());
+        }   
     }
 }
