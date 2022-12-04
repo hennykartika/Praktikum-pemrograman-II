491a59dc4cd3bee8c84fba0a21bf7999ce8dabf4
hennykartika <93182127+hennykartika@users.noreply.github.com>
2022-12-04 23:39:53 +0800

    Update Buku.java

--- Modul 4/Soal1/Buku.java
+++ Modul 4/Soal1/Buku.java
@@ -1,30 +1,23 @@
 package praktikum4.soal1;
 
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        // Deklarasi variabel penampung sementara
-        String judul = "", penulis = "";
-        int tahun = 0;
-        
-        // Instansiasi Scanner dengan nama input
-        Scanner input = new Scanner(System.in);
-        
-        // User input
-        System.out.print("Judul: ");
-        judul = input.nextLine();
-        
-        System.out.print("Penulis: ");
-        penulis = input.nextLine();
-        
-        System.out.print("Tahun Terbit: ");
-        tahun = input.nextInt();
-        
-        // Instansiasi Object Buku dengan nama buku
-        Buku buku = new Buku(judul, penulis, tahun);
-        
-        buku.display();
-        
+public class Buku {
+    private String judul;
+    private String penulis;
+    private Integer tahun;
+    
+    // Constructor
+    public Buku(String j, String p, int t){
+        this.judul = j;
+        this.penulis = p;
+        this.tahun = t;
     }
+    
+    // Method Display Output
+    void display(){
+        System.out.println("Detail buku:");
+        System.out.println("Judul adalah " + this.judul);
+        System.out.println("Penulis adalah " + this.penulis);
+        System.out.println("Tahun Terbit adalah " + this.tahun);
+    }
+    
 }
