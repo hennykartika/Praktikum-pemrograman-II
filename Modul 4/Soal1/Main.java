62f717e994cb5dd26e293378079c1a2ae2a39d88
hennykartika <93182127+hennykartika@users.noreply.github.com>
2022-12-04 23:40:56 +0800

    Update Main.java

--- Modul 4/Soal1/Main.java
+++ Modul 4/Soal1/Main.java
@@ -1,30 +1,30 @@
-package Praktikum4.Soal1;
-import java.util.Scanner;
+package praktikum4.soal1;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        String a, b;
-        int c;
-        /*
-        Urutan Input adalah Judul, Penulis, Tahun Terbit.
-        Penginputan WAJIB menyertakan keterangan yang diikuti dengan tanda ":" dan spasi (" ") input didepannya, seperti:
-        Judul: <Judul Buku>"
-        Penulis: <Nama Penulis>
-        Tahun Terbit: <Tahun Terbit>
-        Contoh Input :
-        Judul: Laskar Pelangi
-        Penulis: Andrea Hirata
-        Tahun Terbit: 2005
-        */
-        inp.next();
-        a = inp.nextLine();
-        inp.next();
-        b = inp.nextLine();
-        inp.next();
-        inp.next();
-        c = inp.nextInt();
-        Buku book = new Buku(a, b, c);
-        book.display();
+        // Deklarasi variabel penampung sementara
+        String judul = "", penulis = "";
+        int tahun = 0;
+        
+        // Instansiasi Scanner dengan nama input
+        Scanner input = new Scanner(System.in);
+        
+        // User input
+        System.out.print("Judul: ");
+        judul = input.nextLine();
+        
+        System.out.print("Penulis: ");
+        penulis = input.nextLine();
+        
+        System.out.print("Tahun Terbit: ");
+        tahun = input.nextInt();
+        
+        // Instansiasi Object Buku dengan nama buku
+        Buku buku = new Buku(judul, penulis, tahun);
+        
+        buku.display();
+        
     }
 }
